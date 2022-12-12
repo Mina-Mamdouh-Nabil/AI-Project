0e24d9d9a48d5b6696ec1617a340653410cabcd6
Mina-Mamdouh-Nabil <minamamdouh.1172001@gmail.com>
2022-12-13 00:10:16 +0200

    I have changed the name of the file to GUI.py

---
+++ AI Project/GUI.py
@@ -0,0 +1,36 @@
+# hello_world.py
+from typing import List, Any
+import io
+import os
+import PySimpleGUI as sg
+from PIL import Image
+file_types = [("JPEG (*.jpg)", "*.jpg"),
+
+              ("All files (*.*)", "*.*")]
+def main():
+    layout = [
+        [sg.Image(key="-IMAGE-")],
+        [
+            sg.Text("Image File"),
+            sg.Input(size=(25, 1), key="-FILE-"),
+            sg.FileBrowse(file_types=file_types),
+            sg.Button("Enter Image"),
+            sg.Button("Predict"),
+        ],
+    ]
+    window = sg.Window("Object detection app", layout, resizable=True)
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if event == "Enter Image":
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                image.thumbnail((400, 400))
+                bio = io.BytesIO()
+                image.save(bio, format="PNG")
+                window["-IMAGE-"].update(data=bio.getvalue())
+    window.close()
+if __name__ == "__main__":
+    main()
